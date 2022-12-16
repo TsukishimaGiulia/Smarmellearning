b285d4985978772066ea325df5a98c4915d31482
Giulia ORRÙ <giulia.orru@alten.it>
2022-12-16 12:13:03 +0100

    aggiunta utility con metodi e main per testarli

---
+++ src/main/Main.java
@@ -0,0 +1,53 @@
+package main;
+
+import domain.*;
+import domain.Animale;
+import utility.Utility;
+
+import java.util.function.Predicate;
+
+public class Main {
+
+    // Deve essere possibile effettuare le seguenti interrogazioni al sistema:
+    // 1. per ogni specie, ricerca dell'esemplare più alto e più basso
+    // 2. per ogni specie, ricerca dell'esemplare più pesante e più leggero
+    // 3. per gli animali dotati di coda, l'esemplare con la coda più lunga di tutto lo zoo nel complesso
+    // 4. per gli animali dotati di ali, l'esemplare con la maggiore apertura alare
+
+    public static void main(String[] args) {
+
+        Predicate<Animale> specieLeone = animale -> animale instanceof Leone;
+        //prendiTigri
+        Predicate<Animale> specieTigre = animale -> animale instanceof Tigre;
+        //prendiAquile
+        Predicate<Animale> specieAquila = animale -> animale instanceof Aquila;
+
+        System.out.println("\n\nElenco animali:");
+        for(Animale a : Utility.caricaAnimali())
+            System.out.println(a);
+
+
+        System.out.println("\n\nLeoni più bassi:");
+        for(Animale a : Utility.animaliPiuBassi(specieLeone))
+            System.out.println(a.toString());
+
+
+        System.out.println("\n\nTigri più alte:");
+        for(Animale a : Utility.animaliPiuAlti(specieTigre))
+            System.out.println(a.toString());
+
+        System.out.println("\n\nAquile più alte:");
+        for(Animale a : Utility.animaliPiuAlti(specieAquila))
+            System.out.println(a.toString());
+
+
+        System.out.println("\n\nAnimali con coda più lunga:");
+        for(AnimaleCodato a : Utility.animaliCodaMax())
+            System.out.println(a.toString());
+
+
+        System.out.println("\n\nAnimali con maggiore apertura alare:");
+        for(AnimaleAlato a :Utility.animaliAperturaAlareMax())
+            System.out.println(a.toString());
+    }
+}
---
+++ src/utility/Utility.java
@@ -0,0 +1,181 @@
+package utility;
+
+import domain.*;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class Utility {
+
+
+    public static Predicate<Animale> specieCodata = animale -> animale instanceof AnimaleCodato;
+
+    public static Predicate<Animale> specieAlata = animale -> animale instanceof AnimaleAlato;
+
+    public static List<Animale> caricaAnimali(){
+        List<Animale> animali = new ArrayList<Animale>();
+
+        Animale l1 = new Leone("gino", "croccantini", 10, new Date(2020,10,10), 100, 50, 20);
+        Animale l2 = new Leone("kimba", "croccantini", 2, new Date(2022,10,10), 10, 15, 10);
+        Animale l3 = new Leone("simba", "croccantini", 12, new Date(2018,10,10), 106.4, 60, 30);
+        Animale l4 = new Leone("pyroar", "baccananas", 10, new Date(2019,10,10), 100, 40, 40);
+
+        animali.add(l1);
+        animali.add(l2);
+        animali.add(l3);
+        animali.add(l4);
+
+        Animale t1 = new Tigre("torakiki", "frittelle", 8, new Date(2020,02,10), 30, 40, 25);
+        Animale t2 = new Tigre("raffaello", "cinghiale", 7, new Date(2016,8,20), 27.2, 40, 20);
+        Animale t3 = new Tigre("dora", "pizza", 2, new Date(2020,9,15), 10, 15, 17);
+
+        animali.add(t1);
+        animali.add(t2);
+        animali.add(t3);
+
+        Animale a1 = new Aquila("leonardo", "pizza", 7, new Date(2016,8,20), 25.7, 37, 32);
+        Animale a2 = new Aquila("nomeACaso", "coniglio", 5, new Date(2019,10,20), 30.4, 30, 40);
+
+        animali.add(a1);
+        animali.add(a2);
+
+        return animali;
+    }
+
+
+    public static List<Animale> getSpecie(Predicate<Animale> specieAnimale){
+        List<Animale> ris = new ArrayList<>();
+        for(Animale a : caricaAnimali())
+            if(specieAnimale.test(a))
+                ris.add(a);
+        return ris;
+    }
+
+    // 1. ALTEZZA
+    public static Set<Animale> animaliPiuAlti(Predicate<Animale>specieAnimale){
+        Set<Animale> ris = new HashSet<>();
+        double max= 0;
+        List<Animale> animali = getSpecie(specieAnimale);
+
+        for(Animale a : animali){
+            if(a.getAltezza()>max)
+                max=a.getAltezza();
+        }
+
+        for(Animale a : animali){
+            if(a.getAltezza()==max)
+                ris.add(a);
+        }
+
+        return ris;
+    }
+
+    public static Set<Animale> animaliPiuBassi(Predicate<Animale>specieAnimale){
+        Set<Animale> ris = new HashSet<>();
+        double min= Double.MAX_VALUE;
+        List<Animale> animali = getSpecie(specieAnimale);
+
+        for(Animale a : animali){
+            if(a.getAltezza()<min)
+                min=a.getAltezza();
+        }
+
+        for(Animale a : animali){
+            if(a.getAltezza()==min)
+                ris.add(a);
+        }
+
+        return ris;
+    }
+
+    // 2. PESO
+    public static Set<Animale> animaliPiuPesanti(Predicate<Animale>specieAnimale){
+        Set<Animale> ris = new HashSet<>();
+        double max= 0;
+        List<Animale> animali = getSpecie(specieAnimale);
+
+        //troviamo il max
+        for(Animale a : animali){
+            if(a.getPeso()>max)
+                max=a.getPeso();
+        }
+
+        //aggiungiamo al set tutti gli animali (di quella specie) per cui peso = max;
+        for(Animale a : animali){
+            if(a.getPeso()==max)
+                ris.add(a);
+        }
+
+        return ris;
+    }
+
+    public static Set<Animale> animaliPiuLeggeri(Predicate<Animale> specieAnimale){
+        Set<Animale> ris = new HashSet<>();
+        List<Animale> animali = getSpecie(specieAnimale);
+        double min = Double.MAX_VALUE;
+
+        //troviamo il min
+        for(Animale a : animali){
+            if(a.getPeso()<min)
+                min=a.getPeso();
+        }
+
+        //aggiungiamo al set tutti gli animali (di quella specie) per cui peso = min;
+        for(Animale a : animali){
+            if(a.getPeso()==min)
+                ris.add(a);
+        }
+
+        // altro modo
+        /*
+        for(Animale a : caricaAnimali()){
+            if(a.getPeso()<min){
+                ris.clear();
+                ris.add(a);
+                min = a.getPeso();
+            } else if (a.getPeso()==min) {
+                ris.add(a);
+            }
+        }*/
+        return ris;
+    }
+
+    // 3. CODA PIU' LUNGA
+    public static Set<AnimaleCodato> animaliCodaMax(){
+        Set<AnimaleCodato> ris = new HashSet<>();
+        double max = 0;
+
+        for(Animale a : getSpecie(specieCodata)){
+            AnimaleCodato ac = (AnimaleCodato)a;
+            if(ac.getLunghezzaCoda()>max){
+                ris.clear();
+                ris.add(ac);
+                max = ac.getLunghezzaCoda();
+            } else if (ac.getLunghezzaCoda()==max) {
+                ris.add(ac);
+            }
+        }
+    return ris;
+
+    }
+
+    // 4. APERTURA ALARE MAGGIORE
+    public static Set<AnimaleAlato> animaliAperturaAlareMax(){
+        Set<AnimaleAlato> ris = new HashSet<>();
+        double max = 0;
+
+        for(Animale a : getSpecie(specieAlata)){
+            AnimaleAlato ac = (AnimaleAlato)a;
+            if(ac.getAperturaAlare()>max){
+                ris.clear();
+                ris.add(ac);
+                max = ac.getAperturaAlare();
+            } else if (ac.getAperturaAlare()==max) {
+                ris.add(ac);
+            }
+        }
+        return ris;
+    }
+
+
+}
