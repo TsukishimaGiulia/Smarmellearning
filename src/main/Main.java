082eedb8f21e40498a8664aa257b495180fb400c
Giulia ORRÙ <giulia.orru@alten.it>
2022-12-19 17:40:30 +0100

    Evidenziata NPE; da correggere

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ import utility.Cerca;
 
 public class Main {
 
-    // Deve essere possibile effettuare le seguenti interrogazioni al sistema:
+    // Deve essere possibile effettuare le seguenti interrogazioni al sistema:f
     // 1. per ogni specie, ricerca dell'esemplare più alto e più basso
     // 2. per ogni specie, ricerca dell'esemplare più pesante e più leggero
     // 3. per gli animali dotati di coda, l'esemplare con la coda più lunga di tutto lo zoo nel complesso
@@ -18,10 +18,10 @@ public class Main {
         for(Animale a : Cerca.mappaAnimali().get("animali"))
             System.out.println(a);
 
-//        System.out.println("\n\nOrsi più pesanti: ");
-//        for(Animale a : Utility.animaliPiuPesanti(specieOrso))
+//        System.out.println("\n\nOrsi più alti:");
+//        for(Animale a : Cerca.animaliPiuAlti("osi"))
 //            System.out.println(a.toString());
-
+//        lancia NPE
 
         System.out.println("\n\nLeoni più bassi:");
         for(Animale a : Cerca.animaliPiuBassi("leoni"))
